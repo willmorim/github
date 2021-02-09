a61b950f892bcf1ac9b70c797c41442d29c8efcf
willmorim <pdbawilliam@gmail.com>
2021-02-08 22:21:15 -0300

    Estilizando Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title, Form } from './styles';
+import { Title, Form, Repositories } from './styles';
 import logoImg from '../../assets/logo-github.svg';
 
 const Dashboard: React.FC = () => {
@@ -13,6 +14,42 @@ const Dashboard: React.FC = () => {
         <input placeholder="digite o nome do repositório" />
         <button type="submit">Pesquisar</button>
       </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/33011070?s=460&u=d548826323e059885477f776d6736adf68eefda7&v=4"
+            alt="William Amorim"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Testando o layout</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/33011070?s=460&u=d548826323e059885477f776d6736adf68eefda7&v=4"
+            alt="William Amorim"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Testando o layout</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/33011070?s=460&u=d548826323e059885477f776d6736adf68eefda7&v=4"
+            alt="William Amorim"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Testando o layout</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
     </>
   );
 };
